4cb3950164834d3573607fe02fb94a9383ec5eb4
camgelozzi <cangelozzi@gmail.com>
2018-03-14 12:21:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# HTML web based game for Starship Troopers anniversary edition
+
+Build with HTML5, CSS3 and Javascript.
+
+Contributors: Me and Jeril Kadvan.
---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Javascript Document
+(() => {
+  console.log("Spot Alien Initiated. Good Luck Soldier!");
+
+// VARIABLES STACK
+let resetButton = document.querySelector('#reset-button');
+let aliensInBlock = ['block1', 'block2', 'block3', 'block4', 'block5', 'block6', 'block7', 'block8'];
+
+// FUNCTIONS STACK
+
+
+// EVENT LISTNERS STACK
+resetButton.addEventListener('click', ()=> console.log("reset button test ok"))
+
+
+})()
